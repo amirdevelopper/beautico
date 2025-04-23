92ac2495b02771cadbfd503fff86b6b9772f0d41
= <amircheshmenooshi@outlook.com>
2025-04-23 20:05:05 +0330

    add to models.py shop

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,179 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.category.name} - {self.name}"
+    
+
+class Color(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    hex_value = models.CharField(max_length=7)  # برای ذخیره کد رنگ به صورت Hex (مثل #FFFFFF)
+
+    def __str__(self):
+        return self.name
+
+class Size(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    subcategory = models.ForeignKey('SubCategory', related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    special_price = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
+    status = models.BooleanField(default=True)
+    
+    # ویژگی‌های جدید
+    colors = models.ManyToManyField(Color, related_name='products', blank=True)
+    sizes = models.ManyToManyField(Size, related_name='products', blank=True)
+    is_special = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def discount_percent(self):
+        if self.special_price and self.special_price < self.price:
+            discount = ((self.price - self.special_price) / self.price) * 100
+            return round(discount)
+        return 0
+    
+
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('در انتظار', 'در انتظار'),
+        ('در حال پردازش', 'در حال پردازش'),
+        ('ارسال شده', 'ارسال شده'),
+        ('تحویل داده شده', 'تحویل داده شده'),
+        ('لغو شده', 'لغو شده'),
+    ]
+
+    customer = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='در انتظار')
+    total_price = models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
+    addres = models.TextField(max_length=200)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
+
+    def calculate_total_price(self):
+        total = sum(item.price * item.stock for item in self.items.all())
+        self.total_price = total
+        self.save()
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    stock = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} x {self.stock}"
+
+
+class Wishlist(models.Model):
+    customer = models.OneToOneField(User, related_name='wishlist', on_delete=models.CASCADE)
+    products = models.ManyToManyField('Product', related_name='wishlists', blank=True)
+
+    def __str__(self):
+        return f"{self.customer.username}'s Wishlist"
+
+
+class Cart(models.Model):
+    customer = models.OneToOneField(User, related_name='cart', on_delete=models.CASCADE)
+    products = models.ManyToManyField('Product', through='CartItem', related_name='carts')
+
+    def __str__(self):
+        return f"{self.customer.username}'s Cart"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.product.name} x {self.stock}"
+    
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()  # بین 1 تا 5
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name} - {self.rating}"
+
+class Shipment(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    tracking_code = models.CharField(max_length=100, blank=True, null=True)
+    carrier = models.CharField(max_length=100)  # نام شرکت پستی
+    shipped_at = models.DateTimeField(blank=True, null=True)
+    delivered_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Shipment for Order #{self.order.id}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=[('male', 'مرد'), ('female', 'زن')], blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Banner(models.Model):
+    link = models.URLField(blank=True, null=True)
+    title = models.CharField(max_length=255, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title or "Banner"
+
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='attributes')
+    name = models.CharField(max_length=100)
+    value = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.name}: {self.value}"
+    
+
+class ProductLimit(models.Model):
+    product = models.ForeignKey(Product, related_name='purchase_limits', on_delete=models.CASCADE)
+    max_purchase_limit = models.IntegerField()
+
+    def __str__(self):
+        return f"Limit for {self.product.name}: {self.max_purchase_limit}"
